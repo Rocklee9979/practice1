c5235fe127babe8bdb183476f0b685697a6689d7
Rocklee9979 <48349639+Rocklee9979@users.noreply.github.com>
2022-01-29 16:14:18 +1100

    installled and used react-stripe-checkout for first time

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/home/Home";
 
 function App() {
+
   return (
     <div className="App">
       <Home/>
+        
     </div>
   );
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './home.scss';
+import StripeCheckout from 'react-stripe-checkout';
 
 export default function Home() {
+
+    
+
+    const handleClick = (token) => {
+        console.log(token);
+    }
     return (
         <div className='container'>
             <h1 className="title">Practicing React Stripe API</h1>
@@ -14,9 +21,20 @@ export default function Home() {
                         It comes with plenty of storage for your favourite books.
                     </p>
                     <span className="price">$100</span>
-                    <button className="productAdd">Buy Now</button>
+                    <StripeCheckout
+                        name="Practice 1"
+                        amount={ 10000 }
+                        stripeKey={ 'pk_test_51KA8MlEkpPuNlofvJfM54jP8e2rvXW0eZBsv6lWtLMRtEl3T29AoFY8Mig9JW1XstTy6UFoGgmeyXcse5B7cKj0I00CYGg763N' }
+                        billingAddress
+                        token={ handleClick }
+                        currency={ 'AUD'}
+                    >
+                        Buy Now
+                    </StripeCheckout>
                 </div>
             </div>
+
+            
         </div>
     );
 }
